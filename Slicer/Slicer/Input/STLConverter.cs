157ddc37174aaa74754ea506dea5c346cf22c937
Guus Kuiper <guus.kuiper@demcon.com>
2021-06-14 19:15:20 +0200

    Add STL writer and add test

--- Slicer/Slicer/Input/STLConverter.cs
+++ Slicer/Slicer/Input/STLConverter.cs
@@ -12,9 +12,20 @@ namespace Slicer.Slicer.Input
     {
         public STL Read(string path)
         {
-            var stl = new STL();
             var file = File.Open(path, FileMode.Open);
+            return ReadBinarySTL(file); 
+        }
+
+        public void Write(string path, STL stl)
+        {
+            var file = File.Open(path, FileMode.CreateNew);
+            WriteBinarySTL(file, stl);
+        }
+
+        public STL ReadBinarySTL(Stream file)
+        {
             using BinaryReader b = new BinaryReader(file, Encoding.ASCII);
+            var stl = new STL();
             stl.Header = new string(b.ReadChars(80));
             stl.NumerOfTriangles = b.ReadUInt32();
             stl.Triangles = new List<Triangle>();
@@ -25,6 +36,20 @@ namespace Slicer.Slicer.Input
             }
 
             return stl;
+        }
+
+        public void WriteBinarySTL(Stream file, STL stl)
+        {
+            using BinaryWriter w = new BinaryWriter(file, Encoding.ASCII);
+            byte[] bytes =  Encoding.ASCII.GetBytes(stl.Header.PadRight(80, '\0'));
+            w.Write(bytes);
+            w.Write(stl.NumerOfTriangles);
+            foreach (Triangle stlTriangle in stl.Triangles)
+            {
+                WriteTriangle(w, stlTriangle);
+            }
+
+            w.Flush();
         }
 
         private Triangle ReadTriangle(BinaryReader b)
@@ -38,6 +63,15 @@ namespace Slicer.Slicer.Input
             return triangle;
         }
 
+        private void WriteTriangle(BinaryWriter w, Triangle t)
+        {
+            WriteVector3(w, t.Normal);
+            WriteVector3(w, t.Vertex1);
+            WriteVector3(w, t.Vertex2);
+            WriteVector3(w, t.Vertex3);
+            w.Write(t.AttributeByteCount);
+        }
+
         private Vector3 ReadVector3(BinaryReader b)
         {
             var v3 = new Vector3();
@@ -45,6 +79,13 @@ namespace Slicer.Slicer.Input
             v3.Y = b.ReadSingle();
             v3.Z = b.ReadSingle();
             return v3;
+        }
+
+        private void WriteVector3(BinaryWriter w, Vector3 v)
+        {
+            w.Write(v.X);
+            w.Write(v.Y);
+            w.Write(v.Z);
         }
     }
 }
---
+++ SlicerTests/Slicer/Input/STLConverterTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using Slicer.Models;
+using Xunit;
+using Slicer.Slicer.Input;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slicer.Slicer.Input.Tests
+{
+    public class STLConverterTests
+    {
+        [Fact]
+        public void ReadWriteTest()
+        {
+            // Arrange
+            var stl = new STL()
+            {
+                Header = "Test STL".PadRight(80),
+                NumerOfTriangles = 1,
+                Triangles = new List<Triangle>()
+                {
+                    new Triangle()
+                    {
+                        Normal = Vector3.UnitZ,
+                        Vertex1 = Vector3.UnitY,
+                        Vertex2 = Vector3.UnitX,
+                        Vertex3 = Vector3.UnitY + Vector3.UnitX,
+                        AttributeByteCount = 0
+                    }
+                }
+            };
+
+            var stlConverter = new STLConverter();
+            var memoryWrite = new MemoryStream();
+
+            // Act
+            stlConverter.WriteBinarySTL(memoryWrite, stl);
+            byte[] buff = memoryWrite.ToArray();
+            
+            var memoryRead = new MemoryStream(buff, false);
+            var readSTL = stlConverter.ReadBinarySTL(memoryRead);
+
+            // Assert 
+            readSTL.Should().BeEquivalentTo(stl);
+        }
+    }
+}
