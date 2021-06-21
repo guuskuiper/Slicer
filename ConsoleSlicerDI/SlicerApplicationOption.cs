8ab10aba38022b7e6bc78fc07092befcef858ee0
guuskuiper <guuskuiper@gmail.com>
2021-06-21 10:38:59 +0200

    Fix arguments

--- ConsoleSlicerDI/SlicerApplicationOption.cs
+++ ConsoleSlicerDI/SlicerApplicationOption.cs
@@ -8,7 +8,7 @@ namespace ConsoleSlicerDI
         public string Outfile { get; init; } = "gcode.gcode";
 
         [Option('i', "input", Required = false, HelpText = "Provide an STL file path.")]
-        public string Infile { get; init; } = "Solid cube 10mm.stl";
+        public string Infile { get; init; } = "Solid cilinder 20mm.stl";
 
         [Option('p', "parallel", Required = false, HelpText = "Slice in parallel.")]
         public bool Parallel { get; init; } = true;
